3a130de45e1e48716f9bce4e79adca7b7b3cb123
Alex Kocharin <alex@kocharin.ru>
2015-03-10 13:35:51 +0300

    Export regexes in index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-// Dummy. Require files directly. 
-module.exports = null;
+
+module.exports.Any = require('./properties/Any/regex');
+module.exports.Cc  = require('./categories/Cc/regex');
+module.exports.Cf  = require('./categories/Cf/regex');
+module.exports.P   = require('./categories/P/regex');
+module.exports.Z   = require('./categories/Z/regex');
